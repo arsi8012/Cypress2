610ea67d51b106e781e85919ec4feb590f840b29
Олег А. Атласов <atlasovoa@kchadm.ru>
2022-03-04 10:37:54 +0400

    cypress test 2

---
+++ cypress/integration/newHall.spec.js
@@ -0,0 +1,12 @@
+const data = require("../fixtures/data.json");
+
+it("Should add two new halls", () => {
+    cy.visit("/admin");
+    cy.login(data.email, data.password);
+    cy.addNewHall(data.newHall1);
+    cy.contains(data.newHall1).should("be.visible");
+    cy.addNewHall(data.newHall2);
+    cy.contains(data.newHall2).should("be.visible");
+    cy.visit("/admin");
+    cy.delNewHall(data.newHall);
+});
